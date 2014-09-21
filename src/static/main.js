dab4f5ab3ea45cfe5498ff534d2d1867e4ff4f81
erpreciso <trepreciso@gmail.com>
2014-09-21 23:35:08 +0200

    recasting to aggregate files

--- src/static/main.js
+++ src/static/main.js
@@ -1,7 +1,12 @@
-
 $(document).ready(init);
-
 function init(){
+        var action = $(".current").attr("id");
+        if (action == "bouncingball") {
+            bouncinganimation();
+        };
+        if (action == "movebrick") {
+            movebrickanimation();
+        };
 	shortcuts();
 	$("input").focus(function(){
 		$(document).off();
@@ -11,7 +16,7 @@ function init(){
 
 function shortcuts(){
 	$(document).keydown(function(event){
-		console.log("key:"+event.which);
+//		console.log("key:"+event.which);
 		href = window.location.href;
 		url = href.substring(0, href.lastIndexOf("/"));
 		if (event.which == 72){
@@ -58,5 +63,108 @@ function drawGrid(ctx, granularity){
 	ctx.stroke();
 	ctx.fillStyle = "red"
 	ctx.fillRect(10,10,10,10);
+}
+
+function bouncinganimation(){
+	var ballcanvas = document.getElementById("canvas");
+	drawBouncingBall(ballcanvas);
+}
+
+function drawBouncingBall (canvas) {
+	
+	var radius = 6;
+	var x =  radius;
+	var y = 0;
+	var maxspeed = 10;
+	var minspeed = 5;
+	var xspeed = generateSpeed(minspeed,maxspeed)
+	var yspeed = generateSpeed(minspeed,maxspeed)
+	function generateSpeed(lowBound,upBound){
+		return Math.floor(Math.random() * (upBound-lowBound)+lowBound);
+	}
+	
+	function animate() {
+		reqAnimFrame = window.requestAnimationFrame ||
+			window.mozRequestAnimationFrame    ||
+			window.webkitRequestAnimationFrame ||
+			window.msRequestAnimationFrame     ||
+			window.oRequestAnimationFrame;
+		reqAnimFrame(animate);
+		x += xspeed;
+		y += yspeed;
+		if(x < radius || x > (canvas.width - radius)){
+			xspeed = (radius + Math.abs(xspeed))*(Math.abs(xspeed)/-xspeed);
+			draw("yellow");
+			x += xspeed;
+			xspeed = generateSpeed(minspeed,maxspeed)*(Math.abs(xspeed)/xspeed);
+		}
+		if(y < radius || y > (canvas.height - radius)){
+			yspeed = (radius + Math.abs(yspeed))*(Math.abs(yspeed)/-yspeed);
+			draw("yellow");
+			y += yspeed;
+			yspeed = generateSpeed(minspeed,maxspeed)*(Math.abs(yspeed)/yspeed);
+		}
+		if (x < (radius+10) || x > (canvas.width-radius-10) ||
+				y < (radius+10) || y > (canvas.height-radius-10)) {
+			draw("yellow");
+		}
+		else{			
+			draw("red");
+		}
+	}
+	function draw(color) {
+		var ctx = canvas.getContext('2d');
+		ctx.clearRect(0, 0, 420, 270);
+		ctx.fillStyle = color;
+		drawArc(ctx, x, y, radius);
+	}
+	animate();
+}
+
+function drawArc(context, xCenter, yCenter, radius){
+	context.beginPath();
+	context.arc(xCenter, yCenter, radius, 0, Math.PI * 2, false);
+	context.fill();
+}
+
+function radians (degrees) {
+	return (Math.PI / 180) * degrees;
+}
+
+function movebrickanimation(){
+	var brickcanvas = document.getElementById("canvas");
+	drawBrick(brickcanvas);
+	var dist = 3;
+	$(document).keydown(function(event){
+		if (event.which >= 37 && event.which <= 40){
+			var direction = event.which - 37; 
+			moveBrick(brickcanvas, direction, dist);
+		}
+	});
+}
+
+function moveBrick(canvas, direction, amount){
+	// 0: left, 1: up, 2: right, 3: down
+	var ctx = canvas.getContext('2d');
+	if (direction == 0){
+		ctx.translate(-amount, 0);
+	}
+	else if (direction == 1){
+		ctx.translate(0, -amount);
+	}
+	else if (direction == 2){
+		ctx.translate(amount, 0);
+	}
+	else if (direction == 3){
+		ctx.translate(0, amount);
+	}
+	drawBrick(canvas);
+}
+function drawBrick(canvas){
+	var ctx = canvas.getContext('2d');
+	ctx.clearRect(0,0,canvas.width,canvas.height);
+	ctx.fillStyle = "red";
+	ctx.fillRect(10,10,20,20);
+	ctx.fill();
 }
 
--- src/trepreciso.py
+++ src/trepreciso.py
@@ -1,23 +1,40 @@
 # coding: utf-8
 # trepreciso webapp
 
+import os
 import webapp2
-import contacts
-import home
-import bouncingball
-import movebrick
-import distancespider
+import jinja2
+import logging
+
+class MainHandler(webapp2.RequestHandler):
+    template_dir = os.path.join(os.path.dirname(__file__), 'pages')
+    jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(
+        template_dir),
+        autoescape = True)
+
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+    
+    def read(self,param):
+        return self.request.get(param)
+        
+    def renderStr(self, template, **params):
+        return self.jinja_env.get_template(template).render(params)
+        
+    def renderPage(self, template, **kw):
+        self.write(self.renderStr(template, **kw))
 
 class JollyHandler(webapp2.RequestHandler):
     def get(self, jolly):
         self.redirect("/home")
-       
+
+class PageHandler(MainHandler):     
+    def get(self, action):
+        logging.info(action)
+        return self.renderPage("index.html", page=action)
+
 PAGE_RE = r'(/(?:[a-zA-Z0-9_-]+/?)*)'
 app = webapp2.WSGIApplication([
-    ("/contacts", contacts.ContactsHandler),
-    ("/home", home.HomeHandler),
-    ("/bouncingball", bouncingball.BouncingBallHandler),
-    ("/movebrick", movebrick.MovebrickHandler),
-    ("/distancespider", distancespider.DistanceSpiderHandler),
+    webapp2.Route(r"/<action>", handler=PageHandler),
     (PAGE_RE, JollyHandler),
             ])
